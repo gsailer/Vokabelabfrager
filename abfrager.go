3cdac5d7cadf8731407f1122b44a2bfe43554f3c
Neo <g051x7db@googlemail.com>
2016-03-29 22:17:26 +0200

    initial commit for go base structure
    just skeleton code and first thoughts in go

---
+++ abfrager.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+)
+
+type vocab struct {
+	bedeutung1, bedeutung2 string
+	abfragen               int
+}
+
+func readVocab(byte []csv) {
+
+}
+
+func main() {
+	args := os.Args
+
+}
